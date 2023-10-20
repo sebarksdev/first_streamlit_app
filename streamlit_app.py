9135f79d8d47b6e5ae281cea71f136d4679ac5df
sebarksdev <84420965+sebarksdev@users.noreply.github.com>
2023-10-20 13:36:24 -0600

    Update streamlit_app.py
    deleted the text output and chnged the url to use variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,7 @@ streamlit.dataframe(fruits_to_show)
 #display fruityvie api response
 streamlit.header('FruityVice Fruit Advice')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # normalize the data into a table format 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
