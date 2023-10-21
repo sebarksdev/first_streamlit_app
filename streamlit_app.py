14e8116742131e639e4cacfc919bb9776009be40
sebarksdev <84420965+sebarksdev@users.noreply.github.com>
2023-10-20 22:20:42 -0600

    Update streamlit_app.py
    closed the connection after inserting the value to the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    mycnx.close()
     streamlit.dataframe(my_data_rows)
 
 
@@ -67,6 +68,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    mycnx.close()
     streamlit.text(back_from_function)
         
 #dont run anything from here
