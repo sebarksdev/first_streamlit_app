ca3f2e572d84fd6c4659fea65e672fb17063da05
sebarksdev <84420965+sebarksdev@users.noreply.github.com>
2023-10-20 21:26:40 -0600

    Update streamlit_app.py
    added a line to insert rows to th snowflake db

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('test')")
